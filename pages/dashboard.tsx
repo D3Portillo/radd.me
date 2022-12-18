5537e2b43d66a7f57e5a64c3680b36e9e45d01fc
D3Portillo <d3portillo@gmail.com>
2022-12-18 12:27:19 +0100

    FEAT: user now can see a skeleton dashboard view

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,14 +1,26 @@
 import { Fragment } from "react"
-import { ConnectButton } from "@rainbow-me/rainbowkit"
+
 import SeoTags from "@/components/SeoTags"
+import Section from "@/components/layout/Section"
+import Navigation from "@/components/Navigation"
+import UserAbout from "@/components/UserAbout/UserAbout"
+import UserPorfolio from "@/components/UserPotfolio"
+import UserCustomLinks from "@/components/UserCustomLinks"
+import UserBanner from "@/components/UserBanner"
+import Footer from "@/components/Footer"
 
 export default function Dashboard() {
   return (
     <Fragment>
-      <SeoTags />
-      <main className="min-h-screen p-8">
-        <ConnectButton />
-      </main>
+      <SeoTags title="Radd / Dashboard" />
+      <UserBanner />
+      <Navigation />
+      <Section isPaddingLess className="min-h-screen px-8 mt-16">
+        <UserAbout />
+        <UserCustomLinks />
+        <UserPorfolio />
+      </Section>
+      <Footer />
     </Fragment>
   )
 }
